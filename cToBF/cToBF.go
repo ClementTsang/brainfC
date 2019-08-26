aa483df593dd2bce3a7b99127df3e2d7c9e84428
ClementTsang <clementjhtsang@gmail.com>
2019-08-25 20:42:46 -0400

    Added most of lexing, but required to fix comment problems and typing

--- cToBF/cToBF.go
+++ cToBF/cToBF.go
@@ -12,9 +12,68 @@ import (
 	"unicode"
 )
 
+// Globals
+
+// Based on http://www.quut.com/c/ANSI-C-grammar-l-2011.html
+const o = "[0-7]"
+const d = "[0-9]"
+const nz = "[1-9]"
+const l = "[a-zA-Z_]"
+const a = "[a-zA-Z_0-9]"
+const h = "[a-fA-F0-9]"
+const hp = "[a-fA-F0-9]"
+const e = "(0[xX])"
+const p = "([Pp][+-]?{D}+)"
+const fs = "(f|F|l|L)"
+const is = "(((u|U)(l|L|ll|LL)?)|((l|L|ll|LL)(u|U)?))"
+const cp = "(u|U|L)"
+const sp = "(u8|u|U|L)"
+const es = "(\\\\(['\"\\?\\\\abfnrtv]|[0-7]{1,3}|x[a-fA-F0-9]+))"
+const ws = "[ \t\v\n\f]"
+
+var validKeywords = []string{
+	"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum",
+	"extern", "float", "for", "goto", "if", "inline", "int", "long", "register", "restrict", "return",
+	"short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void",
+	"volatile", "while",
+}
+
+var underscoredKeywords = []string{
+	"_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic", "_Imaginary", "_Noreturn", "_Static_Assert", "_Thread_local",
+}
+
+const funcName = "__func__"
+
+var validMultiCharSymbols = []string{
+	"...", ">>=", "<<=", "+=", "-=", "*=", "/=", "%=", "&=", "^=", "|=", ">>", "<<", "++", "--", "->", "&&", "||", "<=", ">=", "==", "!=",
+}
+
+var validSingleCharSymbols = []string{
+	";", "{", "}", "<%", "%>", ",", ":", "=", "(", ")", "]", "<:", ":>", "[", ".", "&", "!", "~", "-", "+", "*", "/", "%", "<", ">", "^", "|", "?",
+}
+
+var checkTypeRegex = regexp.MustCompile(l + (a) + "*")
+
+var iConstantRegexOne = regexp.MustCompile(hp + h + "+" + is + "?")
+var iConstantRegexTwo = regexp.MustCompile(nz + d + "*" + is + "?")
+var iConstantRegexThree = regexp.MustCompile("0" + o + "*" + is + "?")
+var iConstantRegexFour = regexp.MustCompile(cp + "?" + "'([^'\\\n]|" + es + ")" + "'")
+
+var fConstantRegexOne = regexp.MustCompile(d + "+" + e + fs + "?")
+var fConstantRegexTwo = regexp.MustCompile(d + "*" + "\".\"" + d + "+" + e + "?" + fs + "?")
+var fConstantRegexThree = regexp.MustCompile(d + "+" + "\".\"" + e + "?" + fs + "?")
+var fConstantRegexFour = regexp.MustCompile(hp + h + "+" + p + fs + "?")
+var fConstantRegexFive = regexp.MustCompile(hp + h + "*" + "\".\"" + h + "+" + p + fs + "?")
+var fConstantRegexSix = regexp.MustCompile(hp + h + "+" + "\".\"" + p + fs + "?")
+
+var stringLiteralRegex = regexp.MustCompile("(" + sp + "?" + "\\\"([^\"\\\\\n]|" + es + ")*\\\"" + ws + "*)+")
+
+//===========================================//
+
 // TODO: Add functions for interface like convertToBF()
 
 type cToken struct {
+	actualVal  string
 	cTokenType string
 }
 
@@ -22,7 +81,58 @@ func genBFCode(tokenSlice []*cToken) (lineSlice []*string) {
 	return
 }
 
+func arrayContains(arr []string, targ string) bool {
+	for _, ele := range arr {
+		if ele == targ {
+			return true
+		}
+	}
+	return false
+}
+
 func getCToken(stringGrouping string) (result *cToken) {
+	//fmt.Printf("Looking for CToken for %s\n", stringGrouping)
+
+	tokenType := "UNKNOWN"
+	if arrayContains(validKeywords, stringGrouping) {
+		tokenType = strings.ToUpper(stringGrouping)
+	} else if arrayContains(underscoredKeywords, stringGrouping) {
+		tokenType = strings.ToUpper(stringGrouping[1:])
+	} else if stringGrouping == funcName {
+		tokenType = "FUNC_NAME"
+	} else if arrayContains(validMultiCharSymbols, stringGrouping) {
+		tokenType = "MUTLICHAR" //TODO: Return correct multicharsymbol
+	} else if arrayContains(validSingleCharSymbols, stringGrouping) {
+		switch stringGrouping {
+		case "<%":
+			tokenType = "{"
+		case "%>":
+			tokenType = "}"
+		case "<:":
+			tokenType = "["
+		case ":>":
+			tokenType = "]"
+		default:
+			tokenType = stringGrouping
+		}
+	} else if checkTypeRegex.MatchString(stringGrouping) {
+		tokenType = "CHKTYPE" //TODO: Checktype
+	} else if iConstantRegexOne.MatchString(stringGrouping) ||
+		iConstantRegexTwo.MatchString(stringGrouping) ||
+		iConstantRegexThree.MatchString(stringGrouping) ||
+		iConstantRegexFour.MatchString(stringGrouping) {
+		tokenType = "I_CONSTANT"
+	} else if fConstantRegexOne.MatchString(stringGrouping) ||
+		fConstantRegexTwo.MatchString(stringGrouping) ||
+		fConstantRegexThree.MatchString(stringGrouping) ||
+		fConstantRegexFour.MatchString(stringGrouping) ||
+		fConstantRegexFive.MatchString(stringGrouping) {
+		tokenType = "F_CONSTANT"
+	} else if stringLiteralRegex.MatchString(stringGrouping) {
+		tokenType = "STRING_LITERAL"
+	}
+
+	result = &cToken{actualVal: stringGrouping, cTokenType: tokenType}
 
 	return
 }
@@ -35,19 +145,16 @@ func isCSymbol(currentRune rune) (result bool) {
 	return
 }
 
-func isValidMultiCharSymbol(currentStringGrouping string, currentRune rune, nextRune rune) (result bool) {
+func isValidMultiCharSymbol(currentStringGrouping string, runeSlice ...rune) (result bool) {
+	currentRune := runeSlice[0]
 	switch currentStringGrouping {
 	case ".":
-		result = (currentRune == '.' && nextRune == '.')
+		result = (currentRune == '.' && len(runeSlice) > 1 && runeSlice[1] == '.')
 	case ">>":
 		fallthrough
 	case "<<":
 		fallthrough
 	case "=":
-		fallthrough
-	case "/":
-		fallthrough
-	case "*":
 		fallthrough
 	case "^":
 		fallthrough
@@ -55,6 +162,10 @@ func isValidMultiCharSymbol(currentStringGrouping string, currentRune rune, next
 		fallthrough
 	case "!":
 		result = (currentRune == '=')
+	case "*":
+		result = (currentRune == '=' || currentRune == '/')
+	case "/":
+		result = (currentRune == '=' || currentRune == '*')
 	case "+":
 		result = (currentRune == '+' || currentRune == '=')
 	case "-":
@@ -71,16 +182,12 @@ func isValidMultiCharSymbol(currentStringGrouping string, currentRune rune, next
 		result = false
 	}
 
-	//fmt.Printf("IsValidMultiCharSymbol with: %s, %c, %c - result: %t\n", currentStringGrouping, currentRune, nextRune, result)
+	//fmt.Printf("IsValidMultiCharSymbol with: %s, %v- result: %t\n", currentStringGrouping, runeSlice, result)
 
 	return
 }
 
 func getTokensForLine(line string) (result []*cToken) {
-
-	// Format and remove unneeded parts:
-	line = regexp.MustCompile("//.*").ReplaceAllString(line, "")
-	fmt.Printf("Regexed string: %s\n", line)
 
 	// Crawl by character!
 	line += " " // Cheat by appending a newline, so the last character in the line will also be output
@@ -89,29 +196,29 @@ func getTokensForLine(line string) (result []*cToken) {
 		if unicode.IsSpace(currentChar) {
 
 			if currentStringGrouping != "" {
-				fmt.Printf("Grouping: %s\n", currentStringGrouping)
+				result = append(result, getCToken(currentStringGrouping))
 			}
 
 			currentStringGrouping = ""
 		} else if isCSymbol(currentChar) {
-			if currentStringGrouping == "" || (currentStringGrouping != "" && pos < len(line) && isValidMultiCharSymbol(currentStringGrouping, currentChar, []rune(line)[pos+1])) {
+			//fmt.Printf("Current symbol grouping: %s, current char: %s\n", currentStringGrouping, string(currentChar))
+
+			if currentStringGrouping == "" || (currentStringGrouping != "" && pos+1 < len(line) && isValidMultiCharSymbol(currentStringGrouping, currentChar, []rune(line)[pos+1])) {
 				// Check to see if this character can continue to form a valid token with the currentStringGrouping
 				currentStringGrouping += string(currentChar)
 
 				if !isCSymbol([]rune(line)[pos+1]) {
-					fmt.Printf("Grouping: %s\n", currentStringGrouping)
+					result = append(result, getCToken(currentStringGrouping))
 					currentStringGrouping = ""
 				}
-
 			} else {
-				// Parse currentStringGrouping and current value of char
-				if currentStringGrouping != "" {
-					fmt.Printf("Grouping: %s\n", currentStringGrouping)
+				result = append(result, getCToken(currentStringGrouping))
+				currentStringGrouping = string(currentChar)
+
+				if !isCSymbol([]rune(line)[pos+1]) {
+					result = append(result, getCToken(currentStringGrouping))
+					currentStringGrouping = ""
 				}
-
-				fmt.Printf("Char: %s\n", string(currentChar))
-
-				currentStringGrouping = ""
 			}
 		} else {
 			currentStringGrouping += string(currentChar)
@@ -137,6 +244,8 @@ func lexC(filePath string) (result []*cToken) {
 			log.Fatal(err)
 			break
 		}
+
+		// Instead, read as a full string, and trim all comments first!
 
 		str = strings.TrimSpace(str) // trim newlines
 
@@ -195,6 +304,10 @@ func ConvertCToBF(inputFile string, toOptimize bool, toFormat bool) {
 	var tokenSlice []*cToken
 
 	tokenSlice = lexC(inputFile)
+
+	for _, token := range tokenSlice {
+		fmt.Printf("Token: %s => %s\n", token.actualVal, token.cTokenType)
+	}
 
 	// Generate BF code and write
 	lineArray = genBFCode(tokenSlice)
--- examples/c_samples/simple_add/simple_add.c
+++ examples/c_samples/simple_add/simple_add.c
@@ -1,7 +1,15 @@
 int main() {
 	int abc = 10+7;//lmao
 	abc -=2;
-	int de24f = abc;
+	int de24f = abc; /* yum*//*lol*/
 	de24f >>=2;
-	return 40 + 2 + abc * 2 - 30;
+
+	/*
+	This is a block comment example
+	abc = 1000;
+	*/
+
+	char* test = /*lmao*/ "banana";
+
+	return 40 + 2 + (abc - 5) * 3 - 30;
 }
